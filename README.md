7d27189b03567c08429f25f0553b288af9f833a2
Jemin Morabiya <jeminmorabiyawork@gmail.com>
2026-07-01 20:17:34 +0530

    Initial File setup and wrote code for generation of kinetic energy matrix(T)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # complex-schrodinger-wavefunction-predictor
-Complex-Valued Neural Network (CVNN) that predicts the eigenfunctions and eigenvalues of the 1D Time-Independent Schrödinger Equation for arbitrary potential energy functions.
+
+# Objective
+Develop a Complex-Valued Neural Network (CVNN) that predicts the eigenfunctions and eigenvalues of the 1D Time-Independent Schrödinger Equation for arbitrary potential energy functions.
+
+# Future plans
+Variable grid sizes
+Adaptive meshes
+Different spatial domains
+Higher dimensions (2D, 3D)
+Neural operators (DeepONet, FNO)
---
+++ dataset/generate.cpp
@@ -0,0 +1,46 @@
+// Units chosen such that ħ²/(2m) = 1. (for simplicity)
+
+#include <vector>
+#include <cnpy>
+
+using Matrix = std::vector<std::vector<double>>;
+
+
+// ===== Global Variables =====
+constexpr int N = 256;
+constexpr double xmin = -5.0;
+constexpr double xmax = 5.0;
+
+
+const double dx = (xmax - xmin)/(N - 1);
+
+int main(){
+    Matrix T(N, std::vector<double>(N, 0.0));
+
+    double inv_dx2 = 1.0 / (dx * dx);
+
+    for (int i = 0; i < N; i++){
+        T[i][i] = -2.0 * inv_dx2;
+
+        if (i > 0) T[i][i - 1] = inv_dx2;
+
+        if (i < N - 1) T[i][i + 1] = inv_dx2;
+    }
+    std::vector<double> flat;
+
+    flat.reserve(N * N);
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+        flat.push_back(T[i][j]);
+        }
+    }
+
+    cnpy::npy_save(
+        "data/T.npy",
+        flat.data(),
+        {static_cast<size_t>(N), static_cast<size_t>(N)},
+        "w"
+    );
+    return 0;
+}
